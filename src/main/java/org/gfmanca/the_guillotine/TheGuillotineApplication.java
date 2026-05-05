41b6d8dbfa6b66650762912f845bf70491129402
Gianfranco <gfman@inwind.it>
2026-05-05 17:46:37 +0200

    First commit

---
+++ src/main/java/org/gfmanca/the_guillotine/TheGuillotineApplication.java
@@ -0,0 +1,13 @@
+package org.gfmanca.the_guillotine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGuillotineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGuillotineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gfmanca/the_guillotine/TheGuillotineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gfmanca.the_guillotine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGuillotineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
